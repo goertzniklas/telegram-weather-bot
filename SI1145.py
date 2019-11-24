86e8ff87c0326ca835abc303f53cc9685f999cf1
Niklas Görtz <37070612+dooflesdoodles@users.noreply.github.com>
2019-11-24 18:07:19 +0100

    Update SI1145.py

--- SI1145.py
+++ SI1145.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python
-
-
-# Author: Joe Gutting
+# original Author: Joe Gutting
 # With use of Adafruit SI1145 library for Arduino, Adafruit_GPIO.I2C & BMP Library by Tony DiCola
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in
-# all copies or substantial portions of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-# THE SOFTWARE.
 
 import logging
 import time
-
 import Adafruit_GPIO.I2C as I2C
 
 # COMMANDS
@@ -261,4 +240,3 @@ class SI1145(object):
         # Returns "Proximity" - assumes an IR LED is attached to LED
         def readProx(self):
                 return self._device.readU16LE(0x26)
-
