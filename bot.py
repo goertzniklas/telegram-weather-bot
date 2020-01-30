77d520f149f5a2c03fe386d96455b1c553bc8140
Niklas G <37070612+goertzniklas@users.noreply.github.com>
2020-01-30 14:28:12 +0100

    enables sensor reading

--- bot.py
+++ bot.py
@@ -1,87 +1,87 @@
 from telegram.ext import Updater
 from telegram.ext import CommandHandler, CallbackQueryHandler, MessageHandler, Filters
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup
-updater = Updater(token='TOKEN', use_context=True)
+import BME280
+from SI1145 import SI1145
+si1145 = SI1145()
+
+updater = Updater(token='859692484:AAHJiQ3FnI-G6NHHoz2KUPIIzUyJxVRk1qg', use_context=True)
 dispatcher = updater.dispatcher
 
+# bot listeners
 def start(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text='Hello {} 👋'.format(update.message.from_user.first_name))
-  context.bot.send_message(chat_id=update.effective_chat.id, text=selection_message(),
-                            reply_markup=selection_keyboard())
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Hallo {} ðŸ‘‹'.format(update.message.from_user.first_name))
+    context.bot.send_message(chat_id=update.effective_chat.id, text=selection_message(), reply_markup=selection_keyboard())
 
 def help(update, context):
-  context.bot.send_message(chat_id=update.effective_chat.id, text=overview_message())
+    context.bot.send_message(chat_id=update.effective_chat.id, text=overview_message())
+
+def summary(update, context):
+    getTemperature(update, context)
+    getHumidity(update, context)
+    getPressure(update, context)
+    getUltraviolet(update, context)
+    getIlluminance(update, context)
+    getInfrared(update, context)
 
 
-def handle_message(update, context):
-    text = update.message.text
-    if text == 'hello':
-        context.bot.send_message(chat_id=update.effective_chat.id, text='Hallo {} 👋'.format(update.message.from_user.first_name))
-
-def selection(bot, update):
-  query = update.callback_query
-  bot.edit_message_text(chat_id=query.message.chat_id,
-                        message_id=query.message.message_id,
-                        text=selection_message(),
-                        reply_markup=first_menu())
-
-class Plant(object):
-    temperature = round(26.76543, 2)
-    summer = ""
-    summer_icon = ""
-    winter = ""
-    winter_icon = ""
-    if temperature >= 25:
-        summer = "Oh and please don't forget to put your suncream on 😎🏝"
-        summer_icon = "☀"
-    elif temperature < 20:
-        winter = "Jacket, scarf and thick socks not that you catch a cold 🙏"
-        winter_icon = "🌨"
-
-
-def temperature_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='The current temperature is {p.temperature}°C {p.summer_icon}{p.winter_icon}'.format(p=Plant()))
-    context.bot.send_message(chat_id=update.effective_chat.id, text='{p.summer}{p.winter}'.format(p=Plant()))
-
-def humidity_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Die Luftfeuchte liegt bei 84%')
-
-def pressure_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Der Luftdruck liegt bei 1000 hPa')
-
-def infrared_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Die Wellenlänge der elektromagnetischen Strahlung liegt bei 800nm')
-
-def illuminance_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Die Lichtstärke beträgt 19.000 lx')
-
-def ultraviolet_button(update, context):
-    context.bot.send_message(chat_id=update.effective_chat.id, text='Die UV-Strahlung liegt bei 215 nm')
-
-def selection_keyboard():
-  keyboard = [[InlineKeyboardButton('Temperatur', callback_data='temperature')],
-              [InlineKeyboardButton('Luftfeuchtigkeit', callback_data='humidity')],
-              [InlineKeyboardButton('Luftdruck', callback_data='pressure')],
-              [InlineKeyboardButton('IR-Strahlung', callback_data='infrared')],
-              [InlineKeyboardButton('Beleuchtungsstärke', callback_data='illuminance')],
-              [InlineKeyboardButton('UV-Strahlung', callback_data='ultraviolet')]]
-
-  return InlineKeyboardMarkup(keyboard)
-
+# bot messages
 def selection_message():
-  return 'What do you want to know about todays weather? 🌤'
+    return 'Was mÃ¶chtest du Ã¼ber das aktuelle Wetter wissen? ðŸŒ¤'
 
 def overview_message():
-    return 'Telegram-Weather-Bot 🌤⛈☀\nI can assist you with the following commands:\n\n▪ /start\t\t\t\t\t\t\t\t\t\t\t\t\t\t\tAn overview of all callable values\n▪ /weather\t\t\t\t\t\t\t\tA summary of your current weather.\n\nFor a description of the Bot visit:\nhttps://google.com'
+    return 'Telegram-Weather-Bot ðŸŒ¤â›ˆâ˜€ \n Ich kann dich mit den folgenden Kommandos unterstÃ¼tzen: \n\n â–ª /start \n Eine Gesamtansicht bekommst du mit: \n â–ª /summary \n A summary of your current weather. \n\n For a description of the Bot visit:\n https://google.com/'
 
+
+# buttons
+def getTemperature(update, context):
+    temperature = BME280.readTemperature()    
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Die aktuelle Temepratur liegt %f Â°C' %temperature )   
+
+def getHumidity(update, context):
+    humidity = BME280.readHumidity()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Die Luftfeuchte liegt bei %f Prozent' %humidity )
+
+def getPressure(update, context):
+    pressure = BME280.readPressure()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Der Luftdruck liegt bei %f hPa' %pressure )
+
+def getUltraviolet(update, context):
+    ultraviolet = si1145.readUV()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Die WellenlÃ¤nge der elektromagnetischen Strahlung liegt bei %f nm' %ultraviolet )
+
+def getIlluminance(update, context):
+    illuminance = si1145.readVisible()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Die LichtstÃ¤rke betrÃ¤gt %f lx' %illuminance )
+
+def getInfrared(update, context):
+    infrared = si1145.readIR()
+    context.bot.send_message(chat_id=update.effective_chat.id, text='Die UV-Strahlung liegt bei %f nm' %infrared)
+
+
+# keyboard
+def selection_keyboard():
+    keyboard = [
+        [InlineKeyboardButton('Temperatur', callback_data='temperature')],
+        [InlineKeyboardButton('Luftfeuchtigkeit', callback_data='humidity')],
+        [InlineKeyboardButton('Luftdruck', callback_data='pressure')],
+        [InlineKeyboardButton('UV-Strahlung', callback_data='ultraviolet')],
+        [InlineKeyboardButton('BeleuchtungsstÃ¤rke', callback_data='illuminance')],
+        [InlineKeyboardButton('IR-Strahlung', callback_data='infrared')]
+    ]
+    return InlineKeyboardMarkup(keyboard)
+
+
+# updater
 updater.dispatcher.add_handler(CommandHandler('start', start))
 updater.dispatcher.add_handler(CommandHandler('help', help))
-updater.dispatcher.add_handler(MessageHandler(filters=Filters.text, callback=handle_message))
-updater.dispatcher.add_handler(CallbackQueryHandler(temperature_button, pattern='temperature'))
-updater.dispatcher.add_handler(CallbackQueryHandler(humidity_button, pattern='humidity'))
-updater.dispatcher.add_handler(CallbackQueryHandler(pressure_button, pattern='pressure'))
-updater.dispatcher.add_handler(CallbackQueryHandler(infrared_button, pattern='infrared'))
-updater.dispatcher.add_handler(CallbackQueryHandler(illuminance_button, pattern='illuminance'))
-updater.dispatcher.add_handler(CallbackQueryHandler(ultraviolet_button, pattern='ultraviolet'))
+updater.dispatcher.add_handler(CommandHandler('summary', summary))
+
+updater.dispatcher.add_handler(CallbackQueryHandler(getTemperature, pattern='temperature'))
+updater.dispatcher.add_handler(CallbackQueryHandler(getHumidity, pattern='humidity'))
+updater.dispatcher.add_handler(CallbackQueryHandler(getPressure, pattern='pressure'))
+updater.dispatcher.add_handler(CallbackQueryHandler(getUltraviolet, pattern='ultraviolet'))
+updater.dispatcher.add_handler(CallbackQueryHandler(getIlluminance, pattern='illuminance'))
+updater.dispatcher.add_handler(CallbackQueryHandler(getInfrared, pattern='infrared'))
 
 updater.start_polling()
